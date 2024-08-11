2f85c182c4ce702ba890b304d799abf1275a6577
Alex-173 <moniuk444@gmail.com>
2024-08-11 18:39:10 +0200

    homework4

---
+++ js/swiper.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper(".swiper-container", {
+	loop: true,
+	slidesPerView: 1,
+	spaceBetween: 16,
+	breakpoints: {
+		768: {
+			slidesPerView: 2,
+		},
+		1240: {
+			slidesPerView: 3,
+		},
+	},
+	pagination: {
+		el: ".pagination",
+		bulletClass: "pagination__button",
+		bulletActiveClass: "pagination__button--active",
+	},
+	navigation: {
+		nextEl: ".services-section__carousel.next",
+		prevEl: ".services-section__carousel.prev",
+	},
+
+	loopAdditionalSlides: 1, // Додаткові слайди для коректного циклічного прокручування
+	centeredSlides: true,
+});
